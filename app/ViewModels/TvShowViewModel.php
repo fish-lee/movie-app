16c69c0084ec9945c675e934ebc90f4f99889ba6
fish-lee <61958394+fish-lee@users.noreply.github.com>
2021-04-13 08:59:06 +0800

    move blade logic to view model
    move some logic to view model
    clean up the blade file
    tidy up blade layouts

--- app/ViewModels/TvShowViewModel.php
+++ app/ViewModels/TvShowViewModel.php
@@ -17,17 +17,23 @@ class TvShowViewModel extends ViewModel
     public function tvshow()
     {
         return collect($this->tvshow)->merge([
-            'poster_path' => 'https://image.tmdb.org/t/p/w500/'.$this->tvshow['poster_path'],
-            'vote_average' => $this->tvshow['vote_average']*10 . '%',
+            'poster_path' => $this->tvshow['poster_path']
+                ? 'https://image.tmdb.org/t/p/w500/'.$this->tvshow['poster_path']
+                : 'https://via.placeholder.com/500x750',
+            'vote_average' => $this->tvshow['vote_average'] * 10 .'%',
             'first_air_date' => Carbon::parse($this->tvshow['first_air_date'])->format('M d, Y'),
             'genres' => collect($this->tvshow['genres'])->pluck('name')->flatten()->implode(', '),
-            'crew' =>collect($this->tvshow['credits']['crew'])->take(2),
-            'cast' =>collect($this->tvshow['credits']['cast'])->take(5),
-            'images' =>collect($this->tvshow['images']['backdrops'])->take(9),
-            //'created_by' =>collect($this->tvshow['created_by'])->take(2),
-        ])
-        ->only([
-            'poster_path', 'id', 'genres', 'name', 'vote_average', 'overview', 'first_air_date', 'credits', 'videos', 'images', 'crew', 'cast', 'created_by'
+            'cast' => collect($this->tvshow['credits']['cast'])->take(5)->map(function($cast) {
+                return collect($cast)->merge([
+                    'profile_path' => $cast['profile_path']
+                        ? 'https://image.tmdb.org/t/p/w300'.$cast['profile_path']
+                        : 'https://via.placeholder.com/300x450',
+                ]);
+            }),
+            'images' => collect($this->tvshow['images']['backdrops'])->take(9),
+        ])->only([
+            'poster_path', 'id', 'genres', 'name', 'vote_average', 'overview', 'first_air_date', 'credits' ,
+            'videos', 'images', 'crew', 'cast', 'images', 'created_by'
         ]);
     }
 }
